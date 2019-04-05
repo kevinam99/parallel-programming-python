49f69402bae30d3f3d450e7d5b40a64952b5760c
kevinam99 <kevinam99@gmail.com>
2019-04-05 19:53:18 +0530

    Process1 checks out.

--- test.py
+++ test.py
@@ -4,6 +4,11 @@ def compute_cube(num):
 def compute_square(num):
     print("%d squared = %d" %(num, num**2))
 
+import multiprocessing
+
+#print(multiprocessing.cpu_count()) #prints number of cores in local machine
 if __name__ == "__main__":
-    compute_cube(7)
-    compute_square(3)
+    process1 = multiprocessing.Process(target = compute_square, args = (4,))
+    process1.start()
+    process1.join() #wait for process1 to complete
+    print("Process1 complete!")
