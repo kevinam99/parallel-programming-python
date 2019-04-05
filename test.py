34be03c34ac254c34115c22f047f62b7f301dba0
kevinam99 <kevinam99@gmail.com>
2019-04-05 19:45:52 +0530

    added single thread square functions. made some more minor edits

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 def compute_cube(num):
-    print("%d cube = %d" %(num, num**3))
+    print("%d cubed = %d" %(num, num**3))
+
+def compute_square(num):
+    print("%d squared = %d" %(num, num**2))
 
 if __name__ == "__main__":
     compute_cube(7)
+    compute_square(3)
