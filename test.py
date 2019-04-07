2891fe030e41848fd49469411e261f866f56cb76
kevinam99 <kevinam99@gmail.com>
2019-04-07 20:53:49 +0530

    Pass 1 complete

--- test.py
+++ test.py
@@ -9,6 +9,13 @@ import multiprocessing
 #print(multiprocessing.cpu_count()) #prints number of cores in local machine
 if __name__ == "__main__":
     process1 = multiprocessing.Process(target = compute_square, args = (4,))
+    process2 = multiprocessing.Process(target = compute_cube, args = (3,))
     process1.start()
+    if(process1.is_alive):
+        print("Process1 begins...")
+    process2.start()
+    if(process2.is_alive):
+        print("Process2 begins...")
     process1.join() #wait for process1 to complete
-    print("Process1 complete!")
+    process2.join() # sames as above
+    print("Processes complete!")
